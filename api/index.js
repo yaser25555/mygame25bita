71ed425ec20f051002de5300c20f883707d47288
اسمك <@.com>
2025-06-29 15:39:24 +0300

    fix(cors): allow Netlify frontend domain in CORS

--- api/index.js
+++ api/index.js
@@ -37,7 +37,8 @@ app.use(cors({
       'http://localhost:3000',
       'http://localhost:5000',
       'http://127.0.0.1:3000',
-      'http://127.0.0.1:5000'
+      'http://127.0.0.1:5000',
+      'https://infinity-box25.netlify.app' // السماح لدومين Netlify
     ];
     
     if (allowedOrigins.indexOf(origin) !== -1) {
